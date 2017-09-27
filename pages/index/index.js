54be01da1f26816bce70ae612a977c40e1b63f24
steven <steven@peipeiyun.com>
2017-09-27 10:11:20 +0800

    删除注释代码

--- pages/index/index.js
+++ pages/index/index.js
@@ -242,94 +242,6 @@ Page({
       "time": 1506395298
     }
   },
-  //事件处理函数
-  // bindViewTap: function() {
-  //   wx.navigateTo({
-  //     url: '../logs/logs'
-  //   })
-  // },
-  // focusInputEvent: function (e) {
-  //   this.setData({
-  //     input_focus: true
-  //   });
-  // },
-  // blurInputEvent:function(e){
-  //   this.setData({
-  //     input_focus: false
-  //   });
-  // },
-  // inputchange: function (e) {
-  //   let s_input = e.detail.value
-  //   let s_show = e.detail.value.replace(/\W/g, "").length !=0 ? true : false
-  //   this.setData({
-  //     inputdata: s_input,
-  //     inputclear: s_show,
-  //   })
-  // },
-
-  // inputClear: function () {
-  //   this.setData({
-  //     inputdata: "",
-  //     inputclear:false
-  //   })
-  // },
-  // goSearch: function (e) {
-  //   let search_input = this.data.inputdata
-  //   let that = this
-  //   if (search_input.length < 1) {
-  //     wx.showToast({
-  //       title: '请输入零件号',
-  //       icon: 'loading',
-  //       duration: 1000
-  //     })
-
-  //     setTimeout(function () {
-  //       wx.hideToast()
-  //     }, 1000)
-  //     return
-  //   }
-  //   let _obj = util.headAdd("/parts/search")
-  //       _obj.parts = search_input
-  //   wx.request({
-  //     url: 'https://beta.007vin.com/parts/search',
-  //     data: _obj,
-  //     method: 'post',
-  //     header: { "Content-Type": "application/x-www-form-urlencoded" },
-  //     success: function (res) {
-  //       if (res.data.data[0].status == 0 || res.data.code == 6){
-  //         wx.showToast({
-  //           title: '无此零件信息',
-  //           icon: 'loading',
-  //           duration: 1000
-  //         })
-  //         setTimeout(function () {
-  //           wx.hideToast()
-  //         }, 1000)
-  //       }else{
-  //         let _savehistory = that.data.savehistory
-  //         let ishav = _savehistory.indexOf(search_input)
-  //         if (ishav == -1) {
-  //           _savehistory.unshift(search_input)
-  //           if (_savehistory.length > 5) {
-  //             _savehistory = _savehistory.slice(0, 5)
-  //           }
-  //           wx.setStorage({
-  //             key: 'savehistory',
-  //             data: _savehistory
-  //           });
-  //         }
-  //         that.setData({
-  //           savehistory: _savehistory,
-  //           history: _savehistory,
-  //           input_focus: false
-  //         })
-  //         wx.navigateTo({
-  //           url: '../resule/index?fortdata=' + res.data.brand + "&pid=" + search_input ,
-  //         })
-  //       }       
-  //     }
-  //   })  
-  // },
   setSelectid(e){
     let _id = e.currentTarget.dataset.id
     this.setData({
@@ -369,45 +281,17 @@ Page({
         }
       })
     }
-
-  var that = this;
-    wx.getStorage({
-      key: 'savehistory',
-      success: function (res) {
-        let _data = res.data
-        that.setData({
-          history: _data,
-          savehistory: _data
-        });
-      }
-    });
-
-    // wx.getClipboardData({
-    //   success: function (res) {
-    //     console.log(res.data)
-    //     let _data = res.data
-    //     var reg = new RegExp("^[A-Za-z0-9]+$")
-    //     if (reg.test(_data)) {
-    //       wx.showModal({
-    //         title: '提示',
-    //         content: '是否粘贴剪切板内容',
-    //         success: function (ress) {
-    //           if (ress.confirm) {
-    //             that.setData({
-    //               input_focus: false,
-    //               inputdata: res.data,
-    //               inputclear: true
-    //             }, () => {
-    //               that.goSearch()
-    //             })
-    //           } else {
-    //             console.log('用户点击取消')
-    //           }
-    //         }
-    //       })
-    //     } 
-    //   }
-    // })
+  // var that = this;
+  //   wx.getStorage({
+  //     key: 'savehistory',
+  //     success: function (res) {
+  //       let _data = res.data
+  //       that.setData({
+  //         history: _data,
+  //         savehistory: _data
+  //       });
+  //     }
+  //   });
   },
   getUserInfo: function(e) {
     app.globalData.userInfo = e.detail.userInfo
